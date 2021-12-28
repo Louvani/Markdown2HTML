6193d9d59bb731fddd6b84eb8c95a623185a37c2
Louvani <paula.louvani@gmail.com>
2021-12-28 12:10:59 -0500

    1. Headings

--- markdown2html.py
+++ markdown2html.py
@@ -4,8 +4,36 @@
 import sys
 from os.path import exists
 
+def read_file(filename=""):
+    '''function to read a file and keep lines in an array'''
+    cp_line = []
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            if line[0] == '#':
+                headings(line, cp_line)
+        return cp_line
+
+def write_file(filename="", cp_line=""):
+    """ function that writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        for line in cp_line:
+            f.write(line)
+
+def headings(line, cp_line):
+    '''Convert Mark down headings into html headings'''
+    level = line.count('#')
+    line = line.replace(('#' * level) + ' ', '<h%s>'%(level))
+    index = line.find('\n')
+    line = line[:index] + '</h%s>'%(level) + line[index:]
+    cp_line.append(line)
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.exit('Usage: ./markdown2html.py README.md README.html')
+    md_name = sys.argv[1]
+    html_name = sys.argv[2]
     if not exists(sys.argv[1]):
-        sys.exit('Missing ' + sys.argv[1])
+        sys.exit('Missing ' + md_name)
+    lines = read_file(md_name)
+    write_file(html_name, lines)
